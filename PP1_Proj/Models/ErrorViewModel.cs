e7615bbbf5ed5444108dc1e0513f6a871bd2880d
laxmi choudhary <eg.laxmi@gmail.com>
2023-12-29 14:45:57 +0530

    Add project files.

---
+++ PP1_Proj/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PP1_Proj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
